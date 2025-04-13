c71bb4db81459505a42f7341a02619646b14b4d3
poti-san <146101605+poti-san@users.noreply.github.com>
2025-04-13 22:27:28 +0900

    Update

--- sphnix/source/conf.py
+++ sphnix/source/conf.py
@@ -3,23 +3,27 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../../src"))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "powguid"
-copyright = "2025, potisan"
-author = "potisan"
+copyright = "2025, poti-san"
+author = "poti-san"
 version = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import sphinx_rtd_theme
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+    "sphinx_rtd_theme",
     "myst_parser",
 ]
 
@@ -27,6 +31,8 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 language = "ja"
+
+napoleon_google_docstring = True
 
 source_suffix = {
     ".rst": "restructuredtext",
